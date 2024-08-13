d1d9fb930cc34c1e6bb7cbfe9320bb7d03e6b00c
czp970118 <493748703@qq.com>
2024-08-14 00:21:06 +0800

    feat:球队列表接口整合

--- express-mysql/app/models/team.model.js
+++ express-mysql/app/models/team.model.js
@@ -6,70 +6,60 @@ class TeamModel {
 	constructor() {
 	}
 
-	getUserFavorTemas = async function (token) {
+	getUserFavorTeams = async function ({ token, userId }) {
 		try {
-			const res = await axios.get(`http://127.0.0.1:7001/user/getUserFavorTeams?token=${token}`);
+			const res = await axios.get(`http://127.0.0.1:7001/user/getUserFavorTeams?token=${token}&userId=${userId}`);
 			return res.data;
 		} catch (error) {
 			throw new Error("Failed to get user favorite teams");
 		}
 	};
 
-	getTeamsWithUserId(params, result) {
-		const { userId, pageSize = 10, pageNum = 1 } = params;
-		if (!userId) {
-			return result({ code: 400, message: 'Missing userId parameter', success: false });
-		}
-		const start = (pageNum - 1) * pageSize;
-		sql.query(`SELECT * FROM user_teams WHERE user_id = ? limit ${pageSize} OFFSET ${start}`, [userId], (err, res) => {
-			if (err) {
-				return result({
-					success: false,
-					message: err.sqlMessage
-				}, null);
-			}
-			const teamIds = res?.map((item) => item.team_id);
-			const teamIdsString = teamIds ? teamIds.join(',') : '';
-			sql.query(`SELECT count(*) totalCount from user_teams  WHERE user_id = ?`, [userId], (err, totalRes) => {
-				if (err) {
-					return result({
-						success: false,
-						message: err.sqlMessage
-					}, null);
-				}
-				sql.query(`SELECT * FROM team WHERE teamId IN (${teamIdsString})`, (err, teamRes) => {
-					if (err) {
-						result({
-							success: false,
-							message: err.sqlMessage
-						}, null);
-					}
-					return result({
-						success: true,
-						message: 'success',
-						data: {
-							list: (teamRes && teamRes.length) ? teamRes.map((item) => {
-								return {
-									...item,
-									favor: true
-								}
-							}) : [],
-							total: totalRes[0].totalCount
-						}
-					});
-				});
-			})
+	getTeamsWithUserId = async function (params) {
+		const { start, pageSize, partition, keywords, teamIdList = [], token, userId } = params
+		const { favorRes } = this.getUserFavorTeams({ token, userId })
+		const teamIdsString = teamIdList.join(',');
 
-		})
-	}
-
-	getTeamList = async function (params) {
-		const { start, pageSize, partition, keywords, isMine } = params
-		let SQL = `SELECT * FROM team `;
+		let SQL = `SELECT * FROM team WHERE teamId IN (${teamIdsString}) `;
 		let values = [];
 
 		if (partition || keywords) {
+			if (partition) {
+				SQL += '`partition` = ? ';
+				values.push(partition);
+			};
+			if (keywords) {
+				if (values.length > 0) {
+					SQL += 'AND ';
+				}
+				SQL += '`teamName` LIKE ? ';
+				values.push(`%${keywords}%`);
+			};
+		}
+
+		return new Promise((resolve, reject) => {
+			sql.query(`${SQL} LIMIT ? OFFSET ?`, [...values, Number(pageSize), start], (err, results) => {
+				if (err) return reject(err);
+				resolve({
+					success: true,
+					data: results
+				});
+			});
+		});
+
+	}
+
+	getTeamList = async function (params) {
+		const { start, pageSize, partition, keywords, isMine, teamIdList = [] } = params
+		let SQL = `SELECT * FROM team `;
+		let values = [];
+
+		if (partition || keywords || isMine === 'true') {
 			SQL += ` WHERE `
+			if (isMine === 'true') {
+				const teamIdsString = teamIdList.join(',')
+				SQL += `teamId IN (${teamIdsString})`
+			}
 			if (partition) {
 				SQL += '`partition` = ? ';
 				values.push(partition);
@@ -113,16 +103,23 @@ class TeamModel {
 			// 验证页码参数
 			if (isNaN(pageSize) || isNaN(pageNum) || pageSize < 1 || pageNum < 1) {
 				throw new Error("Invalid page number or page size");
-			}
-			const [listRes, totalRes, favorTeamIdsRes] = await Promise.all([
-				this.getTeamList({ ...params, start }),
-				this.getTotalCount(),
-				this.getUserFavorTemas(token)
-			])
+			};
 
-			if (listRes.success && totalRes.success && favorTeamIdsRes.success) {
+			const [totalRes, favorRes] = await Promise.all([
+				this.getTotalCount(),
+				this.getUserFavorTeams({ token })
+			]);
+
+			if (totalRes.success && favorRes.success) {
+				const { teamIdList } = favorRes;
+				const queryTemaListParams = {
+					...params,
+					start,
+					teamIdList
+				};
+				const listRes = await this.getTeamList(queryTemaListParams);
 				const newList = listRes.data.map((item) => {
-					return favorTeamIdsRes.teamIdList.includes(item.teamId) ? { ...item, favor: true } : { ...item, favor: false }
+					return teamIdList.includes(item.teamId) ? { ...item, favor: true } : { ...item, favor: false }
 				})
 				result(null, {
 					code: 200,
